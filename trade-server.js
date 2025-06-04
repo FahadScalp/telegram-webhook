b8402cd3ba6d95e3be6fa9df118cf7946e58c08f
FahadScalp <fahadq8trade@gmail.com>
2025-06-04 18:42:23 +0300

    Delete trade-server.js

--- trade-server.js
+++
@@ -1,13 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { handleTradeWebhook } = require('./trade-webhook');
-
-const app = express();
-app.use(bodyParser.json());
-
-app.post('/trade-webhook', handleTradeWebhook);
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`🚀 Trade Webhook شغّال على المنفذ ${PORT}`);
-});
